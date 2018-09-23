68f7d7f8d882f82dc5321ddd5a24a58486de6f14
Unknown <sumire@bc.wakwak.com>
2018-09-23 19:47:03 +0900

    シェーダー用クラスファイルの追加

---
+++ MyGLGame/Shader.cpp
@@ -0,0 +1,9 @@
+//
+//  Shader.cpp
+//  MyGLGame
+//
+//  Created by Katsuhiko Tamura on 2018/09/23.
+//  Copyright © 2018年 Katsuhiko Tamura. All rights reserved.
+//
+
+#include "Shader.hpp"
---
+++ MyGLGame/Shader.hpp
@@ -0,0 +1,14 @@
+//
+//  Shader.hpp
+//  MyGLGame
+//
+//  Created by Katsuhiko Tamura on 2018/09/23.
+//  Copyright © 2018年 Katsuhiko Tamura. All rights reserved.
+//
+
+#ifndef Shader_hpp
+#define Shader_hpp
+
+#include <stdio.h>
+
+#endif /* Shader_hpp */
